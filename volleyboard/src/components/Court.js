bc307d92d5dcd1c762731507f7f9356f720fc8e3
Jake Wilder <jake@Jakes-MacBook-Air.local>
2025-04-15 14:14:23 -0400

    still getting yelled at but productively

---
+++ volleyboard/src/components/Court.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Court() {
+  return (
+    <div className="Court">
+        <div className="Line">
+        </div>
+    </div>
+  );
+}
+
+export default Court;
+
+
---
+++ volleyboard/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Header() {
+  return (
+    <div className="Header">
+        <br />
+        <br />
+    </div>
+  );
+}
+
+export default Header;
+
+
