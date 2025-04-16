6040d11a6cc7f40ad4af974871ab1cbb9267feae
Max Santana <maxsantana@mac.lan>
2025-04-16 18:16:10 -0400

    updated player postions for first 2 rotations

--- volleyboard/src/components/Rotations.js
+++ volleyboard/src/components/Rotations.js
@@ -2,32 +2,49 @@ const rotations = {
   1: {
     default: [
       { id: "S", x: 500, y: 450 },      //Position 1
-      { id: "OH1", x: 500, y: 250 },    //Position 2
-      { id: "M2", x: 400, y: 250 },     //Position 3
-      { id: "OP", x: 300, y: 250 },     //Position 4
-      { id: "OH2", x: 300, y: 450 },    //Position 5
-      { id: "L", x: 400, y: 450 },
+      { id: "OH1", x: 500, y: 100 },    //Position 2
+      { id: "M", x: 325, y: 100 },     //Position 3
+      { id: "OP", x: 150, y: 100 },     //Position 4
+      { id: "OH2", x: 150, y: 450 },    //Position 5
+      { id: "L", x: 325, y: 450 },      //Position 6
     ],
     serve: [
-      { id: 'S', x: 100, y: 300 },
-      { id: 'OH1', x: 200, y: 200 },
-      { id: 'M2', x: 300, y: 200 },
-      { id: 'OP', x: 400, y: 200 },
-      { id: 'OH2', x: 200, y: 300 },
-      { id: 'L', x: 300, y: 300 },
+      { id: "S", x: 500, y: 750 },    //Position 1
+      { id: "OH1", x: 400, y: 100 },     //Position 2
+      { id: "M", x: 325, y: 25 },     //Position 3
+      { id: "OP", x: 250, y: 100 },    //Position 4
+      { id: "OH2", x: 325, y: 575 },      //Position 5
+      { id: "L", x: 500, y: 275 }       //Position 6
     ],
     receive: [
-      { id: 'S', x: 150, y: 300 },
-      { id: 'OH1', x: 200, y: 200 },
-      { id: 'M2', x: 300, y: 200 },
-      { id: 'OP', x: 400, y: 200 },
-      { id: 'OH2', x: 200, y: 300 },
-      { id: 'L', x: 300, y: 300 },
+      { id: "S", x: 580, y: 525 },      //Position 1
+      { id: "OH1", x: 500, y: 450 },    //Position 2
+      { id: "M", x: 75, y: 150 },     //Position 3
+      { id: "OP", x: 15, y: 50 },     //Position 4
+      { id: "OH2", x: 150, y: 450 },    //Position 5
+      { id: "L", x: 325, y: 450 }       //Position 6
     ]
   },
   2: {
-    default: [/* ... */],
-    serve: [/* ... */],
+    default: [
+      { id: "OH1", x: 500, y: 450 },      //Position 1
+      { id: "M", x: 500, y: 100 },    //Position 2
+      { id: "OP", x: 325, y: 100 },     //Position 3
+      { id: "OH2", x: 150, y: 100 },     //Position 4
+      { id: "L", x: 150, y: 450 },    //Position 5
+      { id: "S", x: 325, y: 450 }, 
+    ],
+
+
+    serve: [ 
+      { id: "OH1", x: 500, y: 750 },    //Position 1
+      { id: "M", x: 325, y: 25 },     //Position 2
+      { id: "OP", x: 280, y: 150 },     //Position 3
+      { id: "OH2", x: 150, y: 25 },    //Position 4
+      { id: "L", x: 150, y: 275 },      //Position 5
+      { id: "S", x: 500, y: 275 }        //Position 6
+    ],
+             
     receive: [/* ... */],
   },
   // rotations 3–6...
